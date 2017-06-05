df77ab99cf29373b2232ea30f2c3cfc0a097e398
donker008 <richdonker@qq.com>
2017-06-05 18:39:15 +0800

    add will_pagenite for orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,7 +4,7 @@ class Admin::OrdersController < ApplicationController
 
   def index
     if current_user.is_admin
-      @orders = Order.all.order("created_at")
+      @orders = Order.all.order("created_at").paginate(:page => params[:page], :per_page => 5)
     else
       redirect_to root_path
     end
